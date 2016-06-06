a63d561782ee985306c1783e1e48e3fa82e04e3c
Melvin Carvalho <melvincarvalho@gmail.com>
2016-06-06 07:31:01 +0200

    tabs

--- views/nav.js
+++ views/nav.js
@@ -14,13 +14,27 @@ var nav = `
         </div>
         <!-- Collection of nav links and other content for toggling -->
         <div id="navbarCollapse" class="collapse navbar-collapse">
-            <ul class="nav navbar-nav">
+            <ul class="nav navbar-nav">`
+            if (config.ui && config.ui.tabs) {
+              var tabs = config.ui.tabs
+              for (var key in tabs) {
+                 if (tabs.hasOwnProperty(key)) {
+                   nav += '<li><a href="'+ tabs.key +'">'+ tabs +'</a></li>'
+                 }
+              }
+
+            } else {
+nav += `
                 <li><a href="/">Home</a></li>
                 <li><a href="/balance">Balance</a></li>
                 <li><a href="/random">Content</a></li>
                 <li><a href="/faucet">Faucet</a></li>
                 <li><a href="/deposit">Deposit</a></li>
-                <li><a href="/withdrawal">Withdrawal</a></li>
+                <li><a href="/withdrawal">Withdrawal</a></li>`
+
+            }
+
+nav += `
             </ul>
             <ul class="nav navbar-nav navbar-right">
                 <li><a href="#">Login</a></li>
